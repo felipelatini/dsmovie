a497c15843e4c9e5e58af82ed81b6546877658b2
Felipe Latini Gaudencio <felipelgaudencio@gmail.com>
2022-03-10 14:54:32 -0300

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Listing from './page/Listing/index.js'
+import Form from './page/Form/index.js'
 import Navbar from './components/Navbar/index.js'
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Listing />} />
+        <Route path='/form'>
+          <Route path=':movieId' element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
